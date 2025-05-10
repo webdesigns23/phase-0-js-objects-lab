d4a07f63ba58158981c5d5acdf5b36466e4cd47e
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-05-10 06:28:13 -0600

    added objects to code

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-//Write your code here
+//Declare Object
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+}
 
+//log attendee name
+function logAttendeeName (attendee) {
+      return attendee.name;
+    }
+
+//log ticket price
+function logTicketPrice (attendee) {
+      return attendee.ticketPrice;
+    }
+
+//undate ticket
+function updateTicketType (attendee, newTicketType) {
+      return attendee.ticketType = newTicketType;
+}
+
+//remove the event property
+function removeEventProperty (attendee) {
+      return delete attendee.event;
+}
+
+//Add a checked in property
+function addCheckedInProperty (attendee) {
+      return attendee.checkedIn = true;
+}
 
 
 //Needed for the tests to work. Don't modify
